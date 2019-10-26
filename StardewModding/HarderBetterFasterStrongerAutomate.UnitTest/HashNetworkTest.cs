4cb07d75b69f8eea8d4e002a1e9b087efde0b190
Dan Obermiller <dannnno16@gmail.com>
2019-10-26 10:33:32 -0500

    Basic unit test

---
+++ StardewModding/HarderBetterFasterStrongerAutomate.UnitTest/HashNetworkTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HarderBetterFasterStrongerAutomate.UnitTest
+{
+    [TestClass]
+    public class HashNetworkTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
